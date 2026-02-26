1cf74f366a34f9d9dcc3141379870393f7f711da
joaofeij0 <joaofeijo@ufu.br>
2026-02-26 18:31:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
@@ -22,3 +23,7 @@ export default defineConfig(({mode}) => {
     },
   };
 });
+
+export default defineConfig({
+  base: 'Pontual', 
+})
