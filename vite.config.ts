747aac2bdf497db7d53b948a38ed67860a7f953d
joaofeij0 <joaofeijo@ufu.br>
2026-02-26 18:39:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' 
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
@@ -25,5 +26,6 @@ export default defineConfig(({mode}) => {
 });
 
 export default defineConfig({
+  plugins: [react()],
   base: '/Pontual/', 
 })
